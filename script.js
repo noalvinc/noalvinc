b94fd0709f4fa3ec2ab7bd54bb4bdcc70a73503c
danasbracelets <haziel.morantecruz@gmail.com>
2025-02-14 23:28:08 -0600

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", function() {
     const menu = document.querySelector(".menu");
 
     menuToggle.addEventListener("click", function() {
-        // Cambiar la visibilidad del menú
+        // Alternar la visibilidad del menú
         menu.style.display = menu.style.display === "block" ? "none" : "block";
     });
 });
