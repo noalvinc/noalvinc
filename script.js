98884821ca950f6f700ca5376a77d305de99f097
noalvinc <haziel.morantecruz@gmail.com>
2025-03-05 13:22:46 -0600

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const menuToggle = document.querySelector(".menu-toggle");
-    const menu = document.querySelector(".menu");
-
-    menuToggle.addEventListener("click", function() {
-        // Alternar la visibilidad del menú
-        menu.style.display = menu.style.display === "block" ? "none" : "block";
-    });
-});
